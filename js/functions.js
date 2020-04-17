0dbb49d9fac5ecf164fe13b129105465033fd138
Alberto Annovi <alberto.annovi@cern.ch>
2020-04-17 15:22:40 +0200

    Added the 3rd button driven by the options map

--- js/functions.js
+++ js/functions.js
@@ -7,8 +7,14 @@ function validateForm() {
     return true;
 }
 
+function updateCampaign() {
+    var y = document.getElementById("selectTestSite").selectedIndex;
+    document.getElementById("selectCampaign").innerHTML = opts_Campaign[y];
+}
+
 function updateTestID() {
+    var x = document.getElementById("selectTestSite").selectedIndex;
     var y = document.getElementById("selectCampaign").selectedIndex;
-    document.getElementById("selectTestID").innerHTML = TestIDopts[y];
+    document.getElementById("selectTestID").innerHTML = opts_TestID[x][y];
 }
 
